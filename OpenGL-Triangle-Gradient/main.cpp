20db6dc4be416b4b86c1224dbd89d8d489d0fe89
Aryan Kashyap <matrixapp13@gmail.com>
2016-09-05 12:02:59 +0100

    Initial Commit

---
+++ OpenGL-Triangle-Gradient/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL-Triangle-Gradient
+//
+//  Created by Aryan Kashyap on 05/09/2016.
+//  Copyright © 2016 Aryan Kashyap. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
